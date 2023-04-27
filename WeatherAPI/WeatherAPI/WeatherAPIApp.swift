5c7788489518e5093a0efb35cdd40a7c2bcd864a
Aniket7614 <aniketk561@gmail.com>
2023-04-27 18:16:56 +0530

    Initial commit

---
+++ WeatherAPI/WeatherAPI/WeatherAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPIApp.swift
+//  WeatherAPI
+//
+//  Created by Halcyon Tek on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
